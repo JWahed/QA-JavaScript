190018a2e15d9f535139d03b0e2a67bb1695215b
JWahed <jak.clev@gmail.com>
2022-08-02 10:59:44 +0100

    Create javascript.js

---
+++ getting started with js/javascript.js
@@ -0,0 +1,14 @@
+// Task 1
+console.log("boop");
+console.info("booper");
+console.warn("From dust");
+console.error("Virgo");
+
+// Task 2
+let make = "BMW";
+let model = "i8";
+
+console.log("My favourite car is: " + make + " and the model is: " + model);
+
+// Task 3
+console.log("This is %c test text!", "color: orange; font-family: fantasy; font-style: Bold; background-color: black; padding: 10px");
