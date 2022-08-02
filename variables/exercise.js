64476a27e0178ac59e5df94f5adc1283c10054f3
JWahed <jak.clev@gmail.com>
2022-08-02 11:11:19 +0100

    Create exercise.js

---
+++ variables/exercise.js
@@ -0,0 +1,23 @@
+// Task 1 
+// find js types
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+// Task 2
+// Use template literals in order to dynamically change the sentence
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
