6c6f7efaec14fe1769c479629bb7ddb53b462bd2
JWahed <jak.clev@gmail.com>
2022-08-03 09:46:16 +0100

    Create exercise.js

---
+++ functions, function expressions and arrow functions/exercise.js
@@ -0,0 +1,18 @@
+"use strict";
+// Task 1
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+
+// Task 2
+
+const welcome = function (name, age, gender) {
+    return `My name is ${name}, I am ${age} years old and of gender ${gender}`;
+}
+
+
+// Task 3
+
+const powerUp = (n1, n2) => Math.pow(n1, n2);
